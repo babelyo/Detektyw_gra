658f45c0c2adbe7a9cd05071c027e751f700f84c
student-wroclaw <student-wroclaw@EDU-WSB.local>
2024-05-19 13:38:41 +0200

    into screen

--- cw2_runner_cd.py
+++ cw2_runner_cd.py
@@ -9,6 +9,7 @@ pygame.display.set_caption("Moja pierwsza gra")
 clock = pygame.time.Clock()
 font = pygame.font.SysFont('Arial', 24)  # czcionka systemowa
 player_gravity = 0
+game_active = True
 
 background_surface = pygame.image.load('images_PG/background.png').convert()  # definicja tła
 text_surface = font.render("Punkty: ", 1, "Black")  # definicja napisu
@@ -20,46 +21,70 @@ mashroom_rect = mashroom_surface.get_rect(bottomleft=(520, 350))  # opakowanie s
 player_surface = pygame.image.load('images_PG/girl_stay.png').convert_alpha()
 player_rect = player_surface.get_rect(bottomleft=(50, 350))
 
+player_stay = pygame.image.load('images_PG/girl_stay.png').convert_alpha()
+player_stay = pygame.transform.scale2x(player_stay)
+player_stay_rect = player_stay.get_rect(center=(300, 200))
+
+game_name = font.render("Witaj w mojej pierwszej grze", 0, "White")
+game_name_rect = game_name.get_rect(center=(300, 15))
+
+game_msg = font.render("Naciśnij spację, aby rozpocząć", 0, "White")
+game_msg_rect = game_msg.get_rect(center=(300, 350))
+
 # nieskończona pętla do przytrzymania okna gry
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:  # sprawdzenie jaki to typ zdarzenia
             pygame.quit()  # wyjście z gry
             exit()
-        if event.type == pygame.MOUSEBUTTONDOWN and player_rect.bottom == 350:
-            if player_rect.collidepoint(event.pos):
-                player_gravity = -20
-        if event.type == pygame.KEYDOWN and player_rect.bottom == 350:
-            if event.key == pygame.K_SPACE:
-                # print("Skok")
-                player_gravity = -20
 
-    screen.blit(background_surface, (0, 0))  # wyświetlenie tła
-    #pygame.draw.rect(screen, 'White', text_rect, 2)
-    #pygame.draw.line(screen, "Red", (0, 0), pygame.mouse.get_pos(), 5)
+        if game_active:
+            if event.type == pygame.MOUSEBUTTONDOWN and player_rect.bottom == 350:
+                if player_rect.collidepoint(event.pos):
+                    player_gravity = -20
+            if event.type == pygame.KEYDOWN and player_rect.bottom == 350:
+                if event.key == pygame.K_SPACE:
+                    # print("Skok")
+                    player_gravity = -20
+        else:
+            if event.type == pygame.KEYDOWN and player_rect.bottom == 350:
+                if event.key == pygame.K_SPACE:
+                    game_active = True
+                    mashroom_rect.x = 700
 
-    screen.blit(text_surface, text_rect)  # wyświetlenie napisu
+    if game_active:
+        screen.blit(background_surface, (0, 0))  # wyświetlenie tła
+        #pygame.draw.rect(screen, 'White', text_rect, 2)
+        #pygame.draw.line(screen, "Red", (0, 0), pygame.mouse.get_pos(), 5)
 
-    mashroom_rect.x -= 5
-    if mashroom_rect.x < -100:
-        mashroom_rect.x = 700
+        screen.blit(text_surface, text_rect)  # wyświetlenie napisu
 
-    screen.blit(mashroom_surface, mashroom_rect)
+        mashroom_rect.x -= 5
+        if mashroom_rect.x < -100:
+            mashroom_rect.x = 700
 
-    player_gravity += 1
-    player_rect.y += player_gravity
-    if player_rect.bottom >= 350:
-        player_rect.bottom = 350
-    screen.blit(player_surface, player_rect)
+        screen.blit(mashroom_surface, mashroom_rect)
 
-    # # skakanie - sposób 1
-    # keys = pygame.key.get_pressed()
-    # if keys[pygame.K_SPACE]:
-    #     print("skok")
+        player_gravity += 1
+        player_rect.y += player_gravity
+        if player_rect.bottom >= 350:
+            player_rect.bottom = 350
+        screen.blit(player_surface, player_rect)
 
-    if player_rect.colliderect(mashroom_rect):
-        pygame.quit()
-        exit()
+        # # skakanie - sposób 1
+        # keys = pygame.key.get_pressed()
+        # if keys[pygame.K_SPACE]:
+        #     print("skok")
+
+        if player_rect.colliderect(mashroom_rect):
+            # pygame.quit()
+            # exit()
+            game_active = False
+    else:
+        screen.fill("Black")
+        screen.blit(game_name, game_name_rect)
+        screen.blit(player_stay, player_stay_rect)
+        screen.blit(game_msg, game_msg_rect)
 
     pygame.display.update()  #aktualizacja obraz
     clock.tick(60)  # fps - jak czesto ma sie aktualizowac ekran (60 klatek na sekunde)
